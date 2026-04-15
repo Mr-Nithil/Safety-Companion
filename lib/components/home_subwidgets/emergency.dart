4dd9006c8b5226c2c207227b8dd96397f2b06eff
Mr-Nithil <nithilsheshan@gmail.com>
2026-04-15 11:05:14 +0530

    feat: refactor Emergency component to use PageView for improved navigation

--- lib/components/home_subwidgets/emergency.dart
+++ lib/components/home_subwidgets/emergency.dart
@@ -1,22 +1,57 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:safety_companion/components/home_subwidgets/emergency/ambulance_emergency.dart';
 import 'package:safety_companion/components/home_subwidgets/emergency/army_emergency.dart';
 import 'package:safety_companion/components/home_subwidgets/emergency/firebrigade_emergency.dart';
 import 'package:safety_companion/components/home_subwidgets/emergency/police_emergency.dart';
 
-class Emergency extends StatelessWidget {
+class Emergency extends StatefulWidget {
   const Emergency({super.key});
+
+  @override
+  State<Emergency> createState() => _EmergencyState();
+}
+
+class _EmergencyState extends State<Emergency> {
+  final PageController _pageController = PageController();
+  Timer? _timer;
+  final int _numPages = 4;
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(seconds: 3), (Timer timer) {
+      if (_currentPage < _numPages - 1) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+      if (_pageController.hasClients) {
+        _pageController.animateToPage(
+          _currentPage,
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: MediaQuery.of(context).size.width,
       height: 202,
-      child: ListView(
-        physics: const BouncingScrollPhysics(),
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        scrollDirection: Axis.horizontal,
-        children: [
+      child: PageView(
+        controller: _pageController,
+        children: const [
           PoliceEmergency(),
           AmbulanceEmergency(),
           FirebrigadeEmergency(),
